99b2e753930d47351b35c1554d79e106edc5c769
Mateus Vilione <mateusvilione@gmail.com>
2020-09-13 20:17:37 -0300

    Removendo os using desnecessários

--- PersonWebAPI/Controllers/PersonController.cs
+++ PersonWebAPI/Controllers/PersonController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PersonWebAPI.Data;
 using PersonWebAPI.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Linq;
-using System;
 using PersonWebAPI.ViewModels.PersonViewModels;
 
 namespace PersonWebAPI.Controllers
